0dcbae81e95e00b738947e0ca11377ccc3c74fd5
Rafal <rafalkubala97@gmail.com>
2024-12-30 23:34:00 +0100

    Daisyui import fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
 export default {
   content: [
     "./index.html",
@@ -8,7 +9,6 @@ export default {
     extend: {},
   },
   plugins: [
-    // eslint-disable-next-line no-undef
-    require('daisyui'),
+    daisyui,
   ],
 };
